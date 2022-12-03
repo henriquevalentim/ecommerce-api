ecff3ec63be5e087bd8017c8892f3914231a2fb0
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2022-12-03 17:37:46 -0300

    feat: configure CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const whitelist = ['http://localhost:4200'];
+  app.enableCors({
+    origin: whitelist,
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
